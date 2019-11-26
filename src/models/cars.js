ea720f664d0ff41e751355dd6e213623babb2dda
Jeremy M <jmisiti42@gmail.com>
2019-11-26 20:32:29 +0100

    Modifing name property

--- src/models/cars.js
+++ src/models/cars.js
@@ -1,7 +1,13 @@
 const { Schema, model } = require('mongoose')
 
 const CarsSchema = new Schema({
-    name: { type: String, },
+    name: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        minlength: 3
+    },
     available: { type: Boolean, default: false }
 });
 
--- src/models/stations.js
+++ src/models/stations.js
@@ -1,7 +1,13 @@
 const { Schema, model } = require('mongoose')
 
 const StationsSchema = new Schema({
-    name: { type: String, },
+    name: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        minlength: 3
+    },
     cars: [{ type: Schema.Types.ObjectId, ref: 'Cars' }]
 });
 
